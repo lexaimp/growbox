059c5a4f6286a3b562f5d5ce47cf8ec9b449c0b7
lexaimp <lexaimp1993rus@yandex.ru>
2020-03-20 20:16:32 +0700

    add realtime

--- growbox.ino
+++ growbox.ino
@@ -1,6 +1,8 @@
 #include <DHT.h> // Библиотека для работы с температурным датчиком
 #include <Wire.h> // Библиотека для работы с I2C
 #include <LiquidCrystal_I2C.h> // LCD1602 - Дисплей
+#include <stdio.h>
+#include <DS1302.h> // Библиотека для часов реального времени
 
 #define DHTPIN 4 // Пин температурного датчика
 #define DHTTYPE DHT22 // Тип температурного датчика
@@ -13,12 +15,58 @@ const short resetLed = 13; // HDD светодиод
 // Модули управляющие скоростью вращения куллеров через шим
 const short mosModule1 = 11;
 const short mosModule2 = 10;
+// Пины чясов реального времени
+const short clk = 9;
+const short dat = 8;
+const short rst = 7;
+DS1302 rtc(rst, dat, clk);
+//Получение дня недели
+String dayAsString(const Time::Day day) {
+  switch (day) {
+    case Time::kSunday: return "SU";
+    case Time::kMonday: return "MO";
+    case Time::kTuesday: return "TU";
+    case Time::kWednesday: return "WE";
+    case Time::kThursday: return "TH";
+    case Time::kFriday: return "FR";
+    case Time::kSaturday: return "SA";
+  }
+  return "NO";
+}
+// Функция получения даты
+String getDate(Time tm) {
+  char buf[25];
+  snprintf(buf, sizeof(buf), "%02d-%02d-%04d",
+           tm.date, tm.mon, tm.yr);
+  return buf;
+}
+// Функция получения времени
+String getTime(Time tm) {
+  const String day = dayAsString(tm.day);
+  
+  char buf[25];
+  snprintf(buf, sizeof(buf), "%02d:%02d:%02d %s",
+           tm.hr, tm.min, tm.sec, day.c_str());
+  return buf;
+}
 
-boolean onFlag = false; 
+boolean onFlag = false; // Фллаг для определения нажатия кнопки
 LiquidCrystal_I2C lcd(0x27,16,2);  // Устанавливаем дисплей
 
 DHT dht(DHTPIN, DHTTYPE); // Установка температурного датчика
+
+
 void setup() {
+  
+  /*
+// Установка времени
+  rtc.halt(false); //запуск часов
+  rtc.writeProtect(false); //снять защиту от записи
+// Год, Месяц, День, Время, день недели
+  Time t(2020, 3, 20, 19, 56, 00, Time::kFriday);
+  rtc.time(t);
+  */
+
 // Дисплей
   lcd.init(); // Инициализация                    
   lcd.backlight(); // Включаем подсветку дисплея
@@ -61,12 +109,18 @@ if (digitalRead(onButton) == LOW && onFlag) {
 analogWrite(mosModule2, 250);
 
 // Считывание показаний с температурного датчика
-float t = dht.readTemperature(); // Температура
-float h = dht.readHumidity(); // Влажность
+int t = round(dht.readTemperature()); // Температура
+int h = round(dht.readHumidity()); // Влажность
 
 // Печать данных о температуре-влажности на экран
 lcd.setCursor(2, 0);
 lcd.print(t);
 lcd.setCursor(2, 1);
 lcd.print(h);
+// Печать даты-времени
+Time tm = rtc.time(); // Получение даты-времени
+lcd.setCursor(5, 0);
+lcd.print(getTime(tm));
+lcd.setCursor(5, 1);
+lcd.print(getDate(tm));
 }
